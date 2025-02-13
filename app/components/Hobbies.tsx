0e86fdc63d537c953340b1cc3871691b0415b49b
Quintin Dunn <93884113+quintindunn@users.noreply.github.com>
2025-02-12 21:21:52 -0500

    Added hobby images, fixed magic gradient

--- app/components/Hobbies.tsx
+++ app/components/Hobbies.tsx
@@ -6,8 +6,8 @@ export default function Hobbies() {
         <div id={"hobbies"}>
             <Subheading name={"Hobbies"}/>
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                <Card name={"3D Modeling"} description={"Both aesthetical and functional 3D models."} img_src={""}/>
-                <Card name={"Electronics"} description={"Arduino, & RaspberryPI projects built from the ground up."} img_src={""}/>
+                <Card name={"3D Modeling"} description={"Both aesthetical and functional 3D models."} img_src={"/hobbies/3dmodeling.png"}/>
+                <Card name={"Electronics"} description={"Arduino, & RaspberryPI projects built from the ground up."} img_src={"/hobbies/electronics.png"}/>
             </div>
         </div>
 
