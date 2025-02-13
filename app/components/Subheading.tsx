02a326d5b80ea57294707bbe6993aa21938cd164
Quintin Dunn <93884113+quintindunn@users.noreply.github.com>
2025-02-13 15:10:00 -0500

    Added spacing between subheadings and content

--- app/components/Subheading.tsx
+++ app/components/Subheading.tsx
@@ -2,7 +2,7 @@ export default function Subheading(props: {name: string}) {
     return (
         <a href={`#${props.name}`}>
             <div id={props.name}>
-                <p className={"text-4xl font-semibold"}>{props.name}</p>
+                <p className={"text-4xl font-semibold pb-2"}>{props.name}</p>
             </div>
         </a>
     );
