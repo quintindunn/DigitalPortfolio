bd68bde10776fda4c759cd2c5b9adfb513bbef69
Quintin Dunn <93884113+quintindunn@users.noreply.github.com>
2025-02-12 21:41:21 -0500

    Added profile picture to home page

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import MagicText from "./MagicText";
+import SocialLinks from "@/app/components/SocialLinks";
+import Spacer from "@/app/components/Spacer";
 
 export default function Heading(props: { name: string }) {
     return (
@@ -14,10 +16,18 @@ export default function Heading(props: { name: string }) {
                 />
             </div>
             <h1 className={"inline font-bold text-8xl"}>I&apos;m {props.name}</h1>
-            <div className={"text-4xl indent-5 font-light"}>
-                <p>Software Engineer</p>
-                <p>Professional <MagicText text={"Tinkerer"}/></p>
+            <div className={"grid grid-cols-1 sm:grid-cols-2 gap-8 lg:gap-16"}>
+                <div className={"text-4xl indent-5 font-light"}>
+                    <p>Software Engineer</p>
+                    <p>Professional <MagicText text={"Tinkerer"}/></p>
+                    <Spacer size={20} />
+                    <SocialLinks />
+                </div>
+                <div>
+                    <Image src={"/threadart.png"} alt={"👋"} width={256} height={256}/>
+                </div>
             </div>
+
         </div>
     );
 }
